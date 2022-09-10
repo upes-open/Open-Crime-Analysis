68ac882ccfa90333ca2c767a08fee3badf2f74d9
Med16-11 <78318301+Med16-11@users.noreply.github.com>
2022-09-10 20:48:15 +0530

    updated homepage

--- Side Bars/server.py
+++ Side Bars/server.py
@@ -1,13 +1,38 @@
 from flask import Flask, render_template
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
 
-@app.route('/')
-def index():
-  return render_template('index.html')
 
-@app.route('/India-link/')
-def my_link():
-  return render_template('crime_mapping_sf.html')
+@app.route('/')
+def render_the_map():
+    return render_template('index.html')
+
+@app.route('/dehradun')
+def render_dehradun_map():
+    return render_template('crime_mapping_dehradun.html')
+
+@app.route('/haridwar')
+def render_haridwar_map():
+    return render_template('crime_mapping_haridwar.html')
+
+@app.route('/nanital')
+def render_nanital_map():
+    return render_template('crime_mapping_nanital.html')
+
+@app.route('/cluster')
+def render_cluster_map():
+    return render_template('crime_mapping_marker_cluster.html')
+
+@app.route('/murder')
+def render_murder_map():
+    return render_template('crime_mapping_murder.html')
+
+@app.route('/theft')
+def render_theft_map():
+    return render_template('crime_mapping_theft.html')
+
+@app.route('/heatmap')
+def render_heatmap_map():
+    return render_template('heamap_time.html')
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
